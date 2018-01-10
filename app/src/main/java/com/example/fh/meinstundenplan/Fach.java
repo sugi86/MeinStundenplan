7aa2a2844ea400b6efec331e422f792a45e249ff
sugi86 <sugi86@gmx.de>
2018-01-10 19:55:45 +0100

    Initial commit - Test

---
+++ app/src/main/java/com/example/fh/meinstundenplan/Fach.java
@@ -0,0 +1,135 @@
+package com.example.fh.meinstundenplan;
+/**
+ * Created by Sugi86 on 08.01.2018.
+ */
+
+public class Fach
+{
+    private String semester;
+    private String name;
+    private String tag;
+    private String beginn;
+    private String ende;
+    private String raum;
+    private String dozent;
+    private String kuerzel;
+    private boolean checked;
+
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getBeginn() {
+        return beginn;
+    }
+
+    public void setBeginn(String beginn) {
+        this.beginn = beginn;
+    }
+
+    public String getEnde() {
+        return ende;
+    }
+
+    public void setEnde(String ende) {
+        this.ende = ende;
+    }
+
+    public String getRaum() {
+        return raum;
+    }
+
+    public void setRaum(String raum) {
+        this.raum = raum;
+    }
+
+    public String getDozent() {
+        return dozent;
+    }
+
+    public void setDozent(String dozent) {
+        this.dozent = dozent;
+    }
+
+    public String getKuerzel() {
+        return kuerzel;
+    }
+
+    public void setKuerzel(String kuerzel) {
+        this.kuerzel = kuerzel;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean aktiv) {
+        this.checked = aktiv;
+    }
+
+
+    public Fach(String sem, String n, String t, String b, String e, String r, String doz, String kue, boolean ak)
+    {
+        setSemester(sem);
+        setName(n);
+        setTag(t);
+        setBeginn(b);
+        setEnde(e);
+        setRaum(r);
+        setDozent(doz);
+        setKuerzel(kue);
+        setChecked(ak);
+    }
+
+    public Fach(){
+        setSemester("0");
+        setName("Musterfach");
+        setTag("Sonntag");
+        setBeginn("00:00");
+        setEnde("25:25");
+        setRaum("R666");
+        setDozent("Musterdozent");
+        setKuerzel("MD");
+        setChecked(false);
+    }
+
+    @Override
+    public String toString() {
+        return "Fach{" +
+                "semester='" + semester + '\'' +
+                ", name='" + name + '\'' +
+                ", tag='" + tag + '\'' +
+                ", beginn='" + beginn + '\'' +
+                ", ende='" + ende + '\'' +
+                ", raum='" + raum + '\'' +
+                ", dozent='" + dozent + '\'' +
+                ", kuerzel='" + kuerzel + '\'' +
+                ", aktiv=" + checked +
+                '}';
+    }
+
+    public String CSVtoString() {
+        return semester +";"+name+";"+tag+";"+beginn+";"+ende+";"+raum+";"+dozent+";"+kuerzel+";"+checked+'\n';
+    }
+}
---
+++ app/src/main/java/com/example/fh/meinstundenplan/Stundenplan.java
@@ -0,0 +1,263 @@
+package com.example.fh.meinstundenplan;
+
+import android.content.Context;
+import android.content.EntityIterator;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class Stundenplan extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    public ArrayList<Fach> Faecher = new ArrayList<Fach>();
+
+    public void addFach(String s, String n, String t, String b, String e, String r, String d, String k, boolean ak)
+    {
+        Fach tmpfach = new Fach(s, n, t, b, e, r, d, k, ak);
+        Faecher.add(tmpfach);
+    }
+
+    private void readStundenplan() {
+        InputStream is = getResources().openRawResource(R.raw.ini_ws17);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8") ));
+
+        String line = "";
+        try {
+
+            // Ignore first Line
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                Log.d("MyActivity", "Line: " + line);
+                // Split by ';'
+                String[] tokens = line.split(";");
+
+                //Read the data
+                addFach(tokens[0], tokens[1],tokens[2],tokens[3],tokens[4],tokens[5],tokens[6],tokens[7], false);
+                Log.d("ReadStundenplan", "Just created: " + Faecher.get(Faecher.size() - 1));
+
+                //
+            }
+        }
+        catch (IOException e) {
+            Log.wtf("MyActivity", "Error reading data file on line" + line, e);
+            e.printStackTrace();
+        }
+
+    }
+    public void nichts(){
+        int i = 0;
+        while(i==0)
+        {
+            i++;
+        }
+    }
+
+    private void writeBackup(){
+        try {
+            String state = Environment.getExternalStorageState();
+            if(state.equals(Environment.MEDIA_MOUNTED))
+            {
+                Environment.getExternalStorageDirectory();
+                File externalSD = Environment.getExternalStorageDirectory();
+                File specialExternalDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            }
+
+            SharedPreferences sf = getPreferences(0);
+            SharedPreferences.Editor editor = sf.edit();
+            String tmpstring;
+            for (int i = 0; i < Faecher.size(); i++) {
+                tmpstring = Integer.toString(i);
+                editor.putString(tmpstring,Faecher.get(i).CSVtoString());
+                editor.commit();
+                Log.d("writeSharedPref",  sf.getString(tmpstring,"Keiner!"));
+            }
+            Log.d("SharedPref: ", sf.getString("0", "Failed!"));
+            Log.d("SharedPref: ", sf.getString("1", "Failed!"));
+        }
+        catch(Exception ex) {
+            Log.wtf("InternerSpeicher", "Fehler beim Lesen", ex);
+        }
+    }
+
+    private void readBackup() {
+        SharedPreferences sf = getSharedPreferences("Backup", 0);
+        SharedPreferences.Editor editor = sf.edit();
+        Faecher.clear();
+        int i = 0;
+        String endstring = "Initialized";
+        boolean check;
+        Log.d("Vor While Schleife", "Test: " + endstring);
+        while (true)
+        {
+            endstring = sf.getString(Integer.toString(i), "Keiner!");
+            if (endstring.equals("Keiner!"))
+            {
+                // Log.d("Abbruchbedingung", "Endstring: " + endstring);
+                break;
+            }
+            else {
+                String[] tokens = endstring.split(";");
+                check = Boolean.valueOf(tokens[8]);
+                addFach(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], check);
+                Log.d("ReadSharedPref", "Just created: " + Faecher.get(Faecher.size() - 1));
+                i++;
+                Log.d("In While Schleife", "Zählvariable " + i);
+            }
+        }
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Faecher.size() > 0 )
+        {
+            setContentView(R.layout.activity_stundenplan);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+        else
+        {
+            setContentView(R.layout.activity_stundenplan_leer);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.stundenplan, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.Sidebar_Stundenplan_anzeigen) {
+            readBackup();
+        } else if (id == R.id.Sidebar_Stundenplan_verwalten) {
+
+        } else if (id == R.id.Sidebar_Neues_Fach) {
+            writeBackup();
+        } else if (id == R.id.Sidebar_Einlesen) {
+            readStundenplan();
+
+            setContentView(R.layout.activity_stundenplan);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Einlesen erfolgt", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
